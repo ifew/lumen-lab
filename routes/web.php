32329910a448a0ef4420783a55a85c6f42e97a64
iFew <chitpong@gmail.com>
2018-10-01 18:11:21 +0700

    Changed text for test CI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 $router->get('/', function () use ($router) {
-    return "Hello Lumen Test, and CI/CD";
+    return "Hello Lumen Test, and test CI/CD";
 });
 
 $router->get('version', function () use ($router) {
